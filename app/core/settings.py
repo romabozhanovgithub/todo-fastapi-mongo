63a58939f88ab2a730784d5b91471f33f3762b7d
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-22 21:35:53 +0300

    adjusted settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -7,6 +7,9 @@ load_dotenv("./system_configs/.env")
 
 
 class Settings(BaseSettings):
+    ENV: str = "dev"
+    DEBUG: bool = True
+    APP_TITLE: str = "TODO API"
     DB_HOST: str
     DB_PORT: int
 
