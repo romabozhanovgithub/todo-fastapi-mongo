4f734f44158a8c98a0d054e7ee0575cb51792cc5
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-25 15:52:27 +0300

    add settings for database

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,6 +1,5 @@
-from typing import Optional, Any
-
-from pydantic import BaseSettings
+from typing import Optional
+from pydantic import BaseSettings, validator
 from dotenv import load_dotenv
 
 load_dotenv("./system_configs/.env")
@@ -10,9 +9,19 @@ class Settings(BaseSettings):
     ENV: str = "dev"
     DEBUG: bool = True
     APP_TITLE: str = "TODO API"
+    # DATABASE
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_URL: Optional[str]
+
+    @validator("DB_URL", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: dict[str, str]) -> str:
+        if isinstance(v, str):
+            return v
+        return f"{values['DB_HOST']}{values['DB_USER']}:{values['DB_PASSWORD']}@{values['DB_NAME']}:{values['DB_PORT']}"
 
 
 settings = Settings()
