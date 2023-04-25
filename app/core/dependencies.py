e1bfe9ca32f021a8b3823d174c10f110522c080f
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-25 15:52:42 +0300

    update deps

---
+++ app/core/dependencies.py
@@ -0,0 +1,13 @@
+from fastapi import Depends
+from app.services import MongoService
+from app.services import TaskService
+
+
+def get_mongo_service() -> MongoService:
+    return MongoService()
+
+
+def get_task_service(
+    mongo_service: MongoService = Depends(get_mongo_service)
+) -> TaskService:
+    return TaskService(mongo_service)
