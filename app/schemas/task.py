12a5ab07bb786fca5562e510c3c83e98f9156ba2
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-25 15:50:46 +0300

    update schemas for tasks, add TaskList schema

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -4,45 +4,81 @@ from pydantic import BaseModel, Field
 
 from app.schemas import PyObjectId
 
-schema_extra = {
-    "example": {
-        "title": "Task 1",
-        "description": "Task 1 description",
-        "status": False,
-        "created_at": "2021-01-01 00:00:00"
-    }
-}
-
 
 class TaskConfig:
     allow_population_by_field_name = True
     arbitrary_types_allowed = True
-    json_encoders = {
-        ObjectId: str
-    }
+    json_encoders = {ObjectId: str}
 
 
 class TaskBaseSchema(BaseModel):
     title: str = Field(...)
     description: str = Field(...)
     status: bool = Field(...)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-
 
 class TaskRequestSchema(TaskBaseSchema):
     class Config(TaskConfig):
-        schema_extra = schema_extra
-
-
-class TaskResponseSchema(TaskBaseSchema):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    
-    class Config(TaskConfig):
-        schema_extra = schema_extra.update(
-            {
-                "example": {
-                    "id": "5ff4f9c9e4b0e8b9b8e9f9f9",
-                    **schema_extra["example"]
-                }
+        schema_extra = {
+            "example": {
+                "title": "Task 1",
+                "description": "Task 1 description",
+                "status": False,
             }
-        )
+        }
+
+
+class TaskDBSchema(TaskBaseSchema):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    created_at: datetime = Field(...)
+
+
+class TaskResponseSchema(TaskDBSchema):
+    class Config(TaskConfig):
+        schema_extra = {
+            "example": {
+                "_id": "60c4b8b7e6a9d9f9f7f7f7f7",
+                "title": "Task 1",
+                "description": "Task 1 description",
+                "status": False,
+                "created_at": "2021-01-01 00:00:00",
+            }
+        }
+
+
+class TaskListResponseSchema(BaseModel):
+    tasks: list[TaskResponseSchema] = Field(...)
+
+    class Config(TaskConfig):
+        schema_extra = {
+            "example": {
+                "tasks": [
+                    {
+                        "_id": "60c4b8b7e6a9d9f9f7f7f7f7",
+                        "title": "Task 1",
+                        "description": "Task 1 description",
+                        "status": False,
+                        "created_at": "2021-01-01 00:00:00",
+                    },
+                    {
+                        "_id": "60c4b8b7e6a9d9f9f8f8f8f8",
+                        "title": "Task 2",
+                        "description": "Task 2 description",
+                        "status": True,
+                        "created_at": "2021-01-01 00:00:00",
+                    },
+                ]
+            }
+        }
+
+
+class TaskDeleteResponseSchema(BaseModel):
+    detail: str = "Task deleted successfully"
+    deleted: bool = Field(...)
+
+    class Config(TaskConfig):
+        schema_extra = {
+            "example": {
+                "detail": "Task deleted successfully",
+                "deleted": True,
+            }
+        }
