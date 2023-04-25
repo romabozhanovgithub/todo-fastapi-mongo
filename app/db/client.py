a70350e8d5039043ab19eb26189ecda1b94dfed6
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-25 15:53:16 +0300

    update db connection

--- app/db/client.py
+++ app/db/client.py
@@ -2,4 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.core import settings
 
-client = AsyncIOMotorClient(f"{settings.DB_HOST}:{settings.DB_PORT}")
+client = AsyncIOMotorClient(
+    settings.DB_URL
+)
