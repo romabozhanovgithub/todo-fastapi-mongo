c93879b6fc40cda4794193e0f54241389293d2cc
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-25 15:53:44 +0300

    update mongo service, add typing

--- app/services/mongo.py
+++ app/services/mongo.py
@@ -1,15 +1,24 @@
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
 from app.core import settings
 from app.db import client
 
 
 class MongoService:
-    def __init__(self, client=client, db_name=settings.DB_NAME):
+    def __init__(
+        self,
+        client: AsyncIOMotorClient = client,
+        db_name: str = settings.DB_NAME,
+    ) -> None:
         self.client = client
         self.db_name = db_name
 
-    async def get_db(self):
+    def get_db(self) -> AsyncIOMotorDatabase:
         return self.client[self.db_name]
-    
-    async def get_collection(self, collection_name):
-        db = await self.get_db()
+
+    def get_collection(self, collection_name: str) -> AsyncIOMotorCollection:
+        db = self.get_db()
         return db[collection_name]
