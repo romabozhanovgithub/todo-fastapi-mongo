9e155188c3312d27b202ec4b3b3aa5351fffa35e
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 16:06:34 +0300

    add user service

---
+++ app/services/user.py
@@ -0,0 +1,46 @@
+from app.core.exceptions import UserNotFound
+from app.services.base import BaseService
+from app.schemas import (
+    UserRequestSchema,
+    UserResponseSchema,
+    UserListResponseSchema,
+    UserDBSchema,
+)
+
+
+class UserService(BaseService):
+    collection_name = "users"
+    not_found_exception = UserNotFound
+
+    async def get_users(self) -> UserListResponseSchema:
+        users = await self.find_documents()
+        return UserListResponseSchema(users=users)
+
+    async def get_user(self, user_id: str) -> UserResponseSchema:
+        user = await self.find_document_by_id(user_id)
+        return UserResponseSchema(**user)
+
+    async def get_user_by_email(self, email: str) -> UserDBSchema:
+        user = await self.find_document_by_field("email", email)
+        return UserDBSchema(**user)
+
+    async def create_user(self, user: dict) -> UserResponseSchema:
+        result = await self.create_document(
+            {
+                **user,
+                "is_active": True,
+            }
+        )
+        return UserResponseSchema(**result)
+
+    async def update_user(
+        self, user_id: str, user: UserRequestSchema
+    ) -> UserResponseSchema:
+        result = await self.find_and_update_document_by_id(
+            user_id, user.dict()
+        )
+        return UserResponseSchema(**result)
+
+    async def delete_user(self, user_id: str) -> UserResponseSchema:
+        user = await self.find_and_delete_document_by_id(user_id)
+        return UserResponseSchema(**user)
