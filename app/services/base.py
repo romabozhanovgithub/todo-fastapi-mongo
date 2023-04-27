90b0689100c93d40941678d685cadc3f15ccc886
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 12:52:44 +0300

    add not_found_error and _get_id method

--- app/services/base.py
+++ app/services/base.py
@@ -1,15 +1,24 @@
+from typing import Type
+from bson import ObjectId, errors
 from motor.core import AgnosticCollection
+from app.core.exceptions import BaseHTTPException
 from app.services import MongoService
 
 
 class BaseService:
     collection_name: str
+    not_found_error: Type[BaseHTTPException]
 
-    def __init__(
-        self, mongo_service: MongoService
-    ) -> None:
+    def __init__(self, mongo_service: MongoService) -> None:
         self.mongo_service = mongo_service
         self.collection = self._get_collection()
 
     def _get_collection(self) -> AgnosticCollection:
         return self.mongo_service.get_collection(self.collection_name)
+
+    def _get_id(self, id: str) -> ObjectId:
+        try:
+            object_id = ObjectId(id)
+            return object_id
+        except errors.InvalidId:
+            raise self.not_found_error(id)
--- app/services/task.py
+++ app/services/task.py
@@ -1,41 +1,42 @@
 from datetime import datetime
-from bson import ObjectId
 
-from app.services import BaseService
-from app.schemas import TaskDBSchema, TaskRequestSchema
+from app.services.base import BaseService
+from app.core.exceptions import TaskNotFound
+from app.schemas import (
+    TaskRequestSchema,
+    TaskResponseSchema,
+    TaskListResponseSchema,
+)
 
 
 class TaskService(BaseService):
     collection_name = "tasks"
+    not_found_error = TaskNotFound
 
-    async def get_tasks(self) -> list[TaskDBSchema]:
+    async def get_tasks(self) -> TaskListResponseSchema:
         tasks = await self.collection.find().to_list(length=100)
         return tasks
 
-    async def get_task(self, task_id: str) -> TaskDBSchema:
-        task = await self.collection.find_one({"_id": ObjectId(task_id)})
+    async def get_task(self, task_id: str) -> TaskResponseSchema:
+        task = await self.collection.find_one({"_id": self._get_id(task_id)})
         return task
 
-    async def create_task(self, task: TaskRequestSchema) -> TaskDBSchema:
-        new_task = task.dict()
-        new_task["created_at"] = datetime.utcnow()
-        result = await self.collection.insert_one(new_task)
-        task = await self.get_task(result.inserted_id)
-        return task
+    async def create_task(self, task: TaskRequestSchema) -> TaskResponseSchema:
+        result = await self.collection.insert_one(
+            {**task.dict(), "created_at": datetime.utcnow()}
+        )
+        new_task = await self.get_task(result.inserted_id)
+        return new_task
 
     async def update_task(
         self, task_id: str, task: TaskRequestSchema
-    ) -> TaskDBSchema:
+    ) -> TaskResponseSchema:
         await self.collection.update_one(
-            {"_id": ObjectId(task_id)}, {"$set": task.dict()}
+            {"_id": self._get_id(task_id)}, {"$set": task.dict()}
         )
-        task = await self.get_task(task_id)
-        return task
+        new_task = await self.get_task(task_id)
+        return new_task
 
     async def delete_task(self, task_id: str) -> bool:
-        try:
-            await self.collection.delete_one({"_id": ObjectId(task_id)})
-            return True
-        except Exception as e:
-            print(e)
-            return False
+        await self.collection.delete_one({"_id": self._get_id(task_id)})
+        return True
