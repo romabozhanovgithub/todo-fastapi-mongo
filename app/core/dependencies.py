4b6fb785be219eb6195d6f07081bf41a1c286e14
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 16:08:34 +0300

    add dependecies for user

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
-from app.services import MongoService
-from app.services import TaskService
+from app.services import MongoService, TaskService, UserService, AuthService
+from app.schemas import UserDBBaseSchema
+from app.core.utils import oauth2_scheme
 
 
 def get_mongo_service() -> MongoService:
@@ -11,3 +12,23 @@ def get_task_service(
     mongo_service: MongoService = Depends(get_mongo_service),
 ) -> TaskService:
     return TaskService(mongo_service)
+
+
+def get_user_service(
+    mongo_service: MongoService = Depends(get_mongo_service),
+) -> UserService:
+    return UserService(mongo_service)
+
+
+def get_auth_service(
+    user_service: UserService = Depends(get_user_service),
+) -> AuthService:
+    return AuthService(user_service)
+
+
+async def get_request_user(
+    token: str = Depends(oauth2_scheme),
+    auth_service: AuthService = Depends(get_auth_service),
+) -> UserDBBaseSchema:
+    user = await auth_service.get_current_user(token)
+    return user
