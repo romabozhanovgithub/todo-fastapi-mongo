dea29d91dda5e6ffbc89aab14473c70878ea082a
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 12:50:34 +0300

    add JWT settings and assemble_db_connection validator

--- app/core/settings.py
+++ app/core/settings.py
@@ -16,12 +16,22 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_URL: Optional[str]
+    # JWT
+    JWT_SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 31  # 31 days
 
     @validator("DB_URL", pre=True)
-    def assemble_db_connection(cls, v: Optional[str], values: dict[str, str]) -> str:
+    def assemble_db_connection(
+        cls, v: Optional[str], values: dict[str, str]
+    ) -> str:
         if isinstance(v, str):
             return v
-        return f"{values['DB_HOST']}{values['DB_USER']}:{values['DB_PASSWORD']}@{values['DB_NAME']}:{values['DB_PORT']}"
+        return (
+            f"{values['DB_HOST']}{values['DB_USER']}:{values['DB_PASSWORD']}"
+            f"@{values['DB_NAME']}:{values['DB_PORT']}"
+        )
 
 
 settings = Settings()
