38758540d90339787d30c1ca4d43cff52701d96d
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 21:46:16 +0300

    fix event lopp

--- conftest.py
+++ conftest.py
@@ -1,16 +1,24 @@
+import asyncio
 import pytest
 from app.core.exceptions import BaseHTTPException
 from app.services.base import BaseService
 from app.services.mongo import MongoService
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
 def mongo_service():
     mongo_service = MongoService()
     return mongo_service
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def base_service(mongo_service):
     class NotFoundException(BaseHTTPException):
         status_code = 404
