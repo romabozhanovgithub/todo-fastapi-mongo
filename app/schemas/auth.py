969aa554f52bd95ca4444e919f743fe52628fd7b
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 16:07:35 +0300

    add auth schemas

---
+++ app/schemas/auth.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field
+
+
+class LoginSchema(BaseModel):
+    email: str = Field(..., example="example@gmail.com")
+    password: str = Field(..., example="password")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "example@gmail.com",
+                "password": "password",
+            }
+        }
+
+
+class SignUpSchema(LoginSchema):
+    full_name: str = Field(..., example="User 1")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "full_name": "User 1",
+                "email": "example@gmail.com",
+                "password": "password",
+            }
+        }
+
+
+class TokenSchema(BaseModel):
+    token: str = Field(..., example="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9")
+    token_type: str = Field(..., example="bearer")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                "token_type": "bearer",
+            }
+        }
+
+
+class TokenDataSchema(BaseModel):
+    email: str = Field(..., example="example@gmail.com")
+
+    class Config:
+        schema_extra = {"example": {"email": "example@gmail.com"}}
+
+
+class AccessTokenSchema(BaseModel):
+    access_token: str = Field(
+        ..., example="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+    )
+    token_type: str = Field(..., example="bearer")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                "token_type": "bearer",
+            }
+        }
