6d001bf9002d0a46c98c365515c7f1c7a6433ed5
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 12:51:49 +0300

    add schemas for tasks

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,16 @@
-from app.schemas.helpers import PyObjectId # noqa: F401
+from app.schemas.helpers import PyObjectId  # noqa: F401
+from app.schemas.task import (  # noqa: F401
+    TaskRequestSchema,
+    TaskResponseSchema,
+    TaskListResponseSchema,
+    TaskDeleteResponseSchema,
+    TaskNotFoundResponseSchema,
+    TaskDBSchema,
+)
+from app.schemas.user import (  # noqa: F401
+    UserRequestSchema,
+    UserResponseSchema,
+    UserListResponseSchema,
+    UserDBBaseSchema,
+    UserDBSchema,
+)
