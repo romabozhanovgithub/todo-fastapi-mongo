320f68727d8731edfc615335b935b4f963749cd2
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-04-27 16:08:45 +0300

    register auth router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.core import settings
-from app.routers import task_router
+from app.routers import task_router, auth_router
 
 app = FastAPI(title=settings.APP_TITLE)
 
 app.include_router(task_router)
+app.include_router(auth_router)
